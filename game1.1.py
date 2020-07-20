07cce069fd7f9a2444b992d19636b536dfe46ddb
mrgerwin <jgerwin@otsegoknights.org>
2020-07-20 13:04:15 -0400

    Add files via upload

---
+++ game1.1.py
@@ -0,0 +1,46 @@
+from pygame_functions import *
+
+screenSize(1024,768)
+setAutoUpdate(False)
+link = makeSprite("LinkSimple.png", 14)
+showSprite(link)
+Link_X = 500
+Link_Y = 350
+moveSprite(link, Link_X, Link_Y)
+LINK_SPEED = 4
+
+nextFrame = clock()
+frame = 0
+orientation = 0
+
+while True:
+    if clock() >nextFrame:
+        frame= (frame + 1)%2
+        nextFrame += 80
+        pause(10)
+        
+        if keyPressed("down"):
+            changeSpriteImage(link, 0*2 + frame)
+            orientation =0
+            Link_Y = Link_Y + LINK_SPEED
+        if keyPressed("up"):
+            changeSpriteImage(link, 1*2 + frame)
+            orientation =1
+            Link_Y = Link_Y - LINK_SPEED
+        if keyPressed("right"):
+            changeSpriteImage(link, 2*2 + frame)
+            orientation =2
+            Link_X = Link_X + LINK_SPEED
+        if keyPressed("left"):
+            changeSpriteImage(link, 3*2 + frame)
+            orientation =3
+            Link_X = Link_X - LINK_SPEED
+        if keyPressed("space"):
+            changeSpriteImage(link, orientation + 8)
+        if keyPressed("h"):
+            changeSpriteImage(link, frame+12)
+        
+        moveSprite(link, Link_X, Link_Y)
+        updateDisplay()
+
+endWait()
